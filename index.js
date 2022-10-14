71ff047f94d396102a5e1681a02a90c2333b8cca
Aydar Talantbekov <talantbecovbest@gmail.com>
2022-10-14 15:12:12 +0600

    first committ

---
+++ index.js
@@ -0,0 +1,20 @@
+//! Первое задание
+let num1 = + prompt(`Введите первое число`)
+let num2 = + prompt(`Введите второе число`)
+
+alert(` ${num1+num2}, ${num1*num2}, ${num1-num2}, ${num1/num2}`)
+//! Второе задание
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log(a6, a7, a8, a9, a10);  
+//!Третье задание
+let word1 = 'Hello';
+let word2 = 'World';
+
+let helloworld = word1 + ' ' + word2;
+
+console.log(helloworld);    
